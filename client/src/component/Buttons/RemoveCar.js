0b7fb068da679936b98f4dd9b991d26049e6f533
thalhauzair1 <thalhauzair4@gmail.com>
2024-02-23 11:22:56 -0800

    remove car updated code

--- client/src/component/Buttons/RemoveCar.js
+++ client/src/component/Buttons/RemoveCar.js
@@ -1,27 +1,42 @@
-import { DeleteOutlined } from "@ant-design/icons"
-import { useMutation } from "@apollo/client"
-import { GET_CAR, REMOVE_CAR } from "../../graphql/queries"
+import { DeleteOutlined } from "@ant-design/icons";
+import { useMutation, useQuery } from "@apollo/client";
+import { REMOVE_CAR, GET_CAR, GET_CARS_BY_PEOPLE } from "../../graphql/queries";
 
 const RemoveCar = ({ id }) => {
+  const { loading, error, data } = useQuery(GET_CAR);
 
-    const [removeCar] = useMutation(REMOVE_CAR, {
-        refetchQueries: [{ query: GET_CAR }],
-        awaitRefetchQueries: true
-    })
-    
-    const handleButtonClick = () => {
-        let result = window.confirm('Are you sure you want to delete this Car?')
-    
-        if (result) {
-        removeCar({
-            variables: {
-            id
-            }
-        })
+  const [removeCar] = useMutation(REMOVE_CAR, {
+    update(cache, { data: { removeCar } }) {
+        try {
+          const existingData = cache.readQuery({ query: GET_CARS_BY_PEOPLE, variables: { personId: removeCar.personId } });
+          if (!existingData) return;
+      
+          const updatedCars = existingData.carsOwnedByPeople.filter(car => car.id !== removeCar.id);
+          cache.writeQuery({
+            query: GET_CARS_BY_PEOPLE,
+            variables: { personId: removeCar.personId },
+            data: { carsOwnedByPeople: updatedCars }, 
+          });
+        } catch (error) {
+          console.error('Error updating cache:', error);
         }
-    }
-    
-    return <DeleteOutlined key='delete' style={{ color: 'red' }} onClick={handleButtonClick} />
-    }
+      },
+  });
 
-    export default RemoveCar
+  const handleDelete = () => {
+    const confirmDelete = window.confirm('Are you sure you want to delete this car?');
+    if (confirmDelete) {
+      removeCar({ variables: { id } });
+    }
+  };
+
+  return (
+    <DeleteOutlined
+      key='delete'
+      style={{ color: 'red' }}
+      onClick={handleDelete}
+    />
+  );
+};
+
+export default RemoveCar;
